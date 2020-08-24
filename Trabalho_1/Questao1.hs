b6b44281f8dc98257ebd644dd45c0224f7b11aef
Lucas Sousa <lucas103101@gmail.com>
2020-08-24 20:38:36 -0300

    resolvendo o problema da condição de parada da recursão, pois m >= 5 (parada n == m + 1) e adicionando um menu

--- Trabalho_1/Questao1.hs
+++ Trabalho_1/Questao1.hs
@@ -8,10 +8,40 @@ calc(n, x) = x * ((fatorial(n) * 5) / (fatorial(n-5)*2))
 
 produtorio_CP(m, n)
     |m < 5 = 0
-    |n == m = 1
+    |n == m + 1 = 1
+    |n == m = calc(n, 1)
     |otherwise = ((fatorial(n) * 5) / (fatorial(n-5)*2)) * produtorio_CP(m, n+1)
 
 produtorio_SP(m, n, x)
     |m < 5 = 0
-    |n == m = x
+    |n == m = calc(n, x)
+    |n == m + 1 = x
     |otherwise = produtorio_SP(m, n+1, calc(n, x))
+
+
+lerEntrada = do
+                putStr "Digite um numero, tal que n >= 5 : "
+                x <- getLine        
+                if (read x) < 5
+                    then do
+                        putStr "Numero invalido \n"
+                        lerEntrada
+                else return x
+
+verificaFuncao(m, op)
+    |op == 1 = produtorio_CP(m, 5)
+    |otherwise = produtorio_SP(m, 5, 1)
+
+
+main :: IO()
+main = do
+        print "Escolha o tipo de funcao recursiva pelo numero" 
+        putStr "\n"
+        print "| 1 - Recursao com Pendencia |"
+        print "| 2 - Recursao sem Pendencia |"
+        putStr "\n Digite uma opcao >> "
+        opcao <- getLine
+        m <- lerEntrada
+        putStr "O resultado da operacao eh: "
+        putStr (show(verificaFuncao(read m, read opcao)))
+        putStr "\n"
