1881a3e29d045633d754bc374dff6df258602b00
Jejinketsu <Jederilson@gmail.com>
2020-08-25 12:50:59 -0300

    Modificada Q3 do exercício 1 para não usar listas

--- Exercicio_Fixacao_1/Questao3.hs
+++ Exercicio_Fixacao_1/Questao3.hs
@@ -1,18 +1,20 @@
 module MaiorqMedia where
 
-lerInt message = do
-    putStrLn message
-    a <- getLine
-    return(read a :: Int)
+media(a, b, c) = (a + b + c)/3
 
-media :: [Int] -> Int
-media(list) = div (sum list) (length list)
-
-comparar :: [Int] -> Int
-comparar(list) = length [x | x <- list, x > media(list)]
+comparar(a, b, c)
+    | a > med && b > med && c > med = 3
+    | (a > med && b > med) || (b > med && c > med) || (a > med && c > med) = 2
+    | a > med || b > med || c > med = 1
+    | otherwise = 0
+        where
+            med = media(a, b, c)
 
 main = do
-    a <- lerInt "Digite A: "
-    b <- lerInt "Digite B: "
-    c <- lerInt "Digite C: "
-    print(comparar([a,b,c]))
+    putStr "Digite a: "
+    a <- getLine
+    putStr "Digite b: "
+    b <- getLine
+    putStr "Digite c: "
+    c <- getLine
+    print(comparar(read a, read b, read c))
