14907b735b54402a95924e42d286f5aa4250fca5
Jejinketsu <Jederilson@gmail.com>
2020-08-30 17:28:38 -0300

    Trabalho 1: Correção na 3ª questão

--- Trabalho_1/Questao3.hs
+++ Trabalho_1/Questao3.hs
@@ -3,14 +3,6 @@ module MDC where
 mdc(x, y, divisor)
     | mod x divisor == 0 && mod y divisor == 0 = divisor
     | otherwise = mdc(x, y, divisor-1)
-
-maxNumber(x, y)
-    |x >= y = x
-    |otherwise = y
-
-minNumber(x, y)
-    |x <= y = x
-    |otherwise = y
     
 main :: IO()
 main = do
@@ -20,5 +12,5 @@ main = do
         y <- getLine
         putStr "Digite o segundo valor >> "
         z <- getLine
-        print(minNumber(mdc(read x, read y, maxNumber(read x, read y)), minNumber(mdc(read y, read z, maxNumber(read y, read z)), mdc(read x, read z, maxNumber(read x, read z)))))
+        print(mdc(mdc(read x, read y, read x), mdc(read y, read z, read y), read z))
       
