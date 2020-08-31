4379c1ed638bcf8542d63574da415394c4726201
Jejinketsu <Jederilson@gmail.com>
2020-08-31 10:25:16 -0300

    Trabalho 1: Correção questão 4

--- Trabalho_1/Questao4.hs
+++ Trabalho_1/Questao4.hs
@@ -7,8 +7,8 @@ somaDiv(x, cont)
 
 principal(x)
     |somaDiv(x, 1) == soma = show(x) ++ " eh perfeito, pois " ++ show(x) ++ " * 2 = " ++ show(somaDiv(x, 1)) ++ " , ou seja s(N) = 2*N"
-    |somaDiv(x, 1) < soma = show(x) ++ " eh abundante, pois " ++ show(x) ++ " * 2 > " ++ show(somaDiv(x, 1)) ++ " , ou seja s(N) > 2*N"
-    |somaDiv(x, 1) > soma = show(x) ++ " eh deficiente, pois " ++ show(x) ++ " * 2 < " ++ show(somaDiv(x, 1)) ++ " , ou seja s(N) < 2*N"
+    |somaDiv(x, 1) > soma = show(x) ++ " eh abundante, pois " ++ show(x) ++ " * 2 < " ++ show(somaDiv(x, 1)) ++ " , ou seja s(N) > 2*N"
+    |somaDiv(x, 1) < soma = show(x) ++ " eh deficiente, pois " ++ show(x) ++ " * 2 > " ++ show(somaDiv(x, 1)) ++ " , ou seja s(N) < 2*N"
         where
             soma = x * 2
 
