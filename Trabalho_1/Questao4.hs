d330fea3542d5d74b8478552f7c9b81cf167b352
Lucas Sousa <lucas103101@gmail.com>
2020-09-01 11:34:31 -0300

    permitindo o usuário testar quantos números quiser

--- Trabalho_1/Questao4.hs
+++ Trabalho_1/Questao4.hs
@@ -6,14 +6,22 @@ somaDiv(x, cont)
     | otherwise = somaDiv(x, cont+1)
 
 principal(x)
-    |somaDiv(x, 1) == soma = show(x) ++ " eh perfeito, pois " ++ show(x) ++ " * 2 = " ++ show(somaDiv(x, 1)) ++ " , ou seja s(N) = 2*N"
-    |somaDiv(x, 1) < soma = show(x) ++ " eh abundante, pois " ++ show(x) ++ " * 2 > " ++ show(somaDiv(x, 1)) ++ " , ou seja s(N) > 2*N"
-    |somaDiv(x, 1) > soma = show(x) ++ " eh deficiente, pois " ++ show(x) ++ " * 2 < " ++ show(somaDiv(x, 1)) ++ " , ou seja s(N) < 2*N"
+    |somaDiv(x, 1) == soma = show(x) ++ " eh perfeito, pois " ++ show(somaDiv(x, 1)) ++  " = " ++ show(x) ++ " * 2" ++ " , ou seja s(N) = 2*N"
+    |somaDiv(x, 1) > soma = show(x) ++ " eh abundante, pois " ++ show(somaDiv(x, 1)) ++ " > " ++ show(x) ++ " * 2" ++ " , ou seja s(N) > 2*N"
+    |somaDiv(x, 1) < soma = show(x) ++ " eh deficiente, pois " ++ show(somaDiv(x, 1)) ++ " < " ++ show(x) ++ " * 2" ++ " , ou seja s(N) < 2*N"
         where
             soma = x * 2
 
 main = do
+        putStr "|----- Para finalizar digite 0 -----|"
+        putStr "\n"
         putStr "Digite um numero >> "
         n <- getLine
-        print(principal(read n))
+        if (read n) > 0
+            then do
+                print(principal(read n))
+                putStr "\n"
+                main
+        else
+            print "Programa finalizado"
     
